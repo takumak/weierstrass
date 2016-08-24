f9ece66e2af6352d00c3d954f44ecef9862af666
takumak <deu48803@kwansei.ac.jp>
2016-08-24 12:40:48 +0900

    Improve precision (mathematically)

--- weierstrass.js
+++ weierstrass.js
@@ -1,17 +1,20 @@
+// Issue:
+// * 浮動小数点の精度問題
+//   * fn()中のb^n*pi*xで精度が落ちる
+//   * cx, cyの精度
+
 'use strict';
 
 let conf = {width: 800, height: 600, samples: 1000, a: .5, b: 3};
 
 let curr = {};
 
-let fN = (x, N) => [...Array(N+1).keys()]
-      .map(n=>Math.pow(conf.a,n)*Math.cos(Math.pow(conf.b,n)*Math.PI*x))
-      .reduce((a,b)=>a+b);
+let range = n=>[...Array(n).keys()];
+
+let fn = (x, n) => Math.pow(conf.a,n)*Math.cos(Math.pow(conf.b,n)*Math.PI*x);
+let fN = (x, N) => range(N+1).map(n=>fn(x,n)).reduce((a,b)=>a+b);
 
 let draw = (cx, cy, width, height) => {
-  let N = Math.min(Math.max(Math.floor(100/width), 100), 500);
-  console.log(`${cx},${cy}; ${width}x${height}; N=${N}`);
-
   curr.cx = cx;
   curr.cy = cy;
   curr.width = width;
@@ -22,24 +25,42 @@ let draw = (cx, cy, width, height) => {
   graph.style.width = `${conf.width}px`;
   graph.style.height = `${conf.height}px`;
 
-  let pts = [];
-  for(let gx of Array(conf.samples+1).keys()) {
-    let ax = ((gx - (conf.samples/2)) / (conf.samples/2)) * (width/2) + cx,
-        ay = fN(ax, N),
-        rx = (ax - (cx -  width/2)) / width  * conf.width,
-        ry = (ay - (cy - height/2)) / height * conf.height;
-    pts.push((pts.length ? 'L' : 'M') + `${rx},${conf.height-ry}`);
-  }
 
-  document.getElementById('curve').setAttribute('d', pts.join(' '));
+
+  // n=0..10と1周期が窓の100倍程度(b^n*pi*width*100=2pi)から
+  // conf.samplesで定義される解像度程度(b^n*pi*width/samples=2pi)までのnで近似
+  let imin = Math.floor(Math.log(2/(width*100))/Math.log(conf.b)),
+      imax = Math.ceil(Math.log(2/(width/conf.samples))/Math.log(conf.b));
+
+  console.log(`${cx},${cy}; ${width}x${height}; n=0..10,${imin}..${imax}`);
+
+  document.getElementById('curve').setAttribute(
+    'd',
+    range(conf.samples+1).map(gx => {
+      let ax = ((gx - (conf.samples/2)) / (conf.samples/2)) * (width/2) + cx,
+          ay = fN(ax, 10),
+          rx = (ax - (cx -  width/2)) / width  * conf.width,
+          ry = (ay - (cy - height/2)) / height * conf.height;
+
+      if (imax > 10) {
+        for(let i = Math.max(imin, 11); i < imax; i++) {
+          ry += fn(ax, i) / height * conf.height;
+        }
+      }
+
+      return (gx ? 'L' : 'M') + `${rx},${conf.height-ry}`;
+    }).join(' ')
+  );
 };
 
 draw(0, 0, 4, 4);
 
+
+let calcpos = (x, y) => [(x -  conf.width/2)/( conf.width/2),
+                         (y - conf.height/2)/(conf.height/2)*(-1)];
+
 document.getElementById('graph').addEventListener('wheel', ev => {
-  let rect = ev.target.getBoundingClientRect(),
-      mx =  (ev.clientX -  rect.width/2)/( rect.width/2),
-      my = -(ev.clientY - rect.height/2)/(rect.height/2),
+  let [mx, my] = calcpos(ev.clientX, ev.clientY),
       scale = ev.deltaY < 0 ? .5 : 2,
       w = scale * curr.width,
       h = scale * curr.height,
@@ -47,4 +68,25 @@ document.getElementById('graph').addEventListener('wheel', ev => {
       cy = my * (curr.height / 2) + curr.cy - my*(w/2);
 
   draw(cx, cy, w, h);
+});
+
+
+let dragging;
+
+document.getElementById('graph').addEventListener('mousedown', ev => {
+  dragging = [[ev.clientX, ev.clientY], [curr.cx, curr.cy]];
+  ev.preventDefault();
+});
+
+document.getElementById('graph').addEventListener('mouseup', ev => {
+  dragging = null;
+});
+
+document.getElementById('graph').addEventListener('mousemove', ev => {
+  if (dragging) {
+    let [[mx, my], [cx, cy]] = dragging,
+        ox = (mx - ev.clientX) *  curr.width /  conf.width,
+        oy = (my - ev.clientY) * curr.height / conf.height;
+    draw(cx + ox, cy - oy, curr.width, curr.height);
+  }
 });
